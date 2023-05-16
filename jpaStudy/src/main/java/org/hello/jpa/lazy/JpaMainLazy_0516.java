94320e957c3d8e045607fdbe12e3269aad5e06a3
kkimsungchul <kimsc1218@gmail.com>
2023-05-16 17:41:20 +0900

    2023.05.16
    JPA 공부

--- jpaStudy/src/main/java/org/hello/jpa/lazy/JpaMainLazy_0516.java
+++ jpaStudy/src/main/java/org/hello/jpa/lazy/JpaMainLazy_0516.java
@@ -1,11 +1,14 @@
-package org.hello.jpa.proxy;
+package org.hello.jpa.lazy;
 
 import org.hello.jpa.member.domain.Member;
-import org.hibernate.Hibernate;
+import org.hello.jpa.member.domain.Team;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
-public class JpaMainProxy_0516 {
+public class JpaMainLazy_0516 {
     public static void main(String[]args){
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
@@ -15,22 +18,23 @@ public class JpaMainProxy_0516 {
         entityTransaction.begin();
 
         try{
+            Team team = new Team();
+            team.setName("TEAM_A");
+            entityManager.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("hello1");
+            member1.setTeam(team);
             entityManager.persist(member1);
+
 
             entityManager.flush();
             entityManager.clear();
 
             Member findMember1 = entityManager.getReference(Member.class , member1.getId());
-//            System.out.println(findMember1.getClass());
-//            System.out.println("## isLoaded : " + entityManagerFactory.getPersistenceUnitUtil().isLoaded(findMember1));
-//            System.out.println(findMember1.getUsername());
-//            System.out.println("## isLoaded : " + entityManagerFactory.getPersistenceUnitUtil().isLoaded(findMember1));
-//            System.out.println(findMember1.getClass());
-//            System.out.println(findMember1.getClass().getName());
-
-            //Hibernate.initialize(findMember1);
+            System.out.println(findMember1.getUsername());
+            System.out.println(findMember1.getTeam().getClass());
+            System.out.println(findMember1.getTeam().getName());
 
             entityTransaction.commit();
         }catch (Exception e){
