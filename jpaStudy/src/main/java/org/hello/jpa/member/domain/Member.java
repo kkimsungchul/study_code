dbda76f5397b31d24923f93533e40df8d06ece3b
kimsc1218 <kimsc1218@gmail.com>
2023-06-02 23:46:00 +0900

    2023.06.02 JPA study 코드 업로드

--- jpaStudy/src/main/java/org/hello/jpa/member/domain/Member.java
+++ jpaStudy/src/main/java/org/hello/jpa/member/domain/Member.java
@@ -1,6 +1,10 @@
 package org.hello.jpa.member.domain;
 
+import org.hello.jpa.member.domain.embedded.Address;
+import org.hello.jpa.member.domain.embedded.Period;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 
 @Entity //필수
@@ -12,6 +16,21 @@ public class Member extends BaseEntity{
 
     @Column(name = "USERNAME")
     private String username;
+
+    @Embedded
+    private Period workPeriod;
+
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city" , column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name="street" , column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name="zipcode" , column = @Column(name = "WORK_ZIPCODE")),
+    })
+    private Address workAddress;
+
 
 //    @Column(name="TEAM_ID")
 //    private Long teamId;
@@ -51,6 +70,5 @@ public class Member extends BaseEntity{
         this.team = team;
         team.getMembers().add(this);
     }
-
 
 }
