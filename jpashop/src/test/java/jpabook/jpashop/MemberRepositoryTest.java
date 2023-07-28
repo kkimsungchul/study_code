716488c924426289278e028dd035dae611bd953c
kkimsungchul <kimsc1218@gmail.com>
2023-07-28 17:46:29 +0900

    2023.07.28 - jpa 공부 코드 작성

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)//테스트 후 데이터 롤백 안함
+    public void testMember(){
+
+        //given
+        Member member = new Member();
+        member.setUsername("김성철");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.findMember(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        Assertions.assertThat(findMember).isEqualTo(member);//같음, 같은 영속성 컨텍스트 안에 있음
+        System.out.println("### findMember == member : " +  (findMember == member));
+
+
+
+
+    }
+
+
+}
