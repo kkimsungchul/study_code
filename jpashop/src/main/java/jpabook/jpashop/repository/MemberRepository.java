fbf3358b6ef99a50b10f57ac58b3b13ef7c29d79
kimsungchul <kimsc1218@gmail.com>
2023-09-19 18:15:59 +0900

    2023.09.19- jpa member repository 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.repository;
+
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    };
+
+
+
+}
