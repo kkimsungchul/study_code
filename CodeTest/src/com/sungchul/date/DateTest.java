bda05b4bbf9d7b00aa304b44df8e3a97e4c5325e
kimsc1218 <kimsc1218@gmail.com>
2023-10-14 01:00:39 +0900

    2023.10.14 - Date 테스트 코드 작성

--- CodeTest/src/com/sungchul/date/DateTest.java
+++ CodeTest/src/com/sungchul/date/DateTest.java
@@ -1,13 +1,67 @@
 package com.sungchul.date;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DateTest {
     public static void main(String[]args){
-        System.out.println(dayEqual("2023-04-30"));
-        System.out.println(isDayBeforeNow("2023-04-30"));
+//        System.out.println(dayEqual("2023-04-30"));
+//        System.out.println(isDayBeforeNow("2023-04-30"));
 
+//        String time = "2023-10-13 14:22:32.15333";
+//        System.out.println("### time  : " + time);
+//        String changeTime = changeDateFormat(time,"yyyy-MM-dd HH:mm:ss.SSSSS","yyyy-MM-dd HH:mm:ss");
+//        System.out.println("### Time  : " + changeTime);
+
+        String time = "2023-10-13 14:22:32.271";
+        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS"); // 수정된 패턴
+        SimpleDateFormat newDtFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date formatDate = null;
+        try {
+            formatDate = dtFormat.parse(time);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("### time : " + time);
+        System.out.println("###change Time : " + newDtFormat.format(formatDate));
+
+//
+//        String time = "2023-10-13 14:22:32.15333";
+//        DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSS");
+//        LocalDateTime dateTime = LocalDateTime.parse(time, dtFormatter);
+//
+//        DateTimeFormatter newDtFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        System.out.println("### time : " + time);
+//        System.out.println("### change Time : " + dateTime.format(newDtFormatter));
+//
+//        String dateString = "2023-10-13 14:22:32";
+//        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        try {
+//            Date parsedDate = dtFormat.parse(dateString);
+//            System.out.println("Parsed Date: " + parsedDate);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        Date currentDate = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String formattedDate = simpleDateFormat.format(currentDate);
+//        System.out.println("Formatted Date: " + formattedDate);
     }
+
+    public static String changeDateFormat(String date, String nowFormat, String changeFormat) {
+        DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern(nowFormat);
+        LocalDateTime dateTime = LocalDateTime.parse(date, dtFormatter);
+
+        DateTimeFormatter newDtFormatter = DateTimeFormatter.ofPattern(changeFormat);
+        return dateTime.format(newDtFormatter);
+    }
+
+
     public static boolean dayEqual(String day){
         String[] splitString = day.split("-");
         LocalDate getDate = LocalDate.of(
