84082e86aa8ae3356da530873122dd518c6599b5
kimsungchul <kimsc1218@gmail.com>
2024-01-05 17:33:14 +0900

    2024.01.05
    1. order api 개선 시작

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,14 +1,19 @@
 package jpabook.jpashop.api;
 
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -31,7 +36,41 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();//Lazy 강제 초기화
         }
         return all;
+    }
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        //ORDER 2개
+        // N +1 > 1 + 회원N + 배송N
+//        쿼리가 총 1 + N + N번 실행된다. (v1과 쿼리수 결과는 같다.)
+//        order 조회 1번(order 조회 결과 수가 N이 된다.)
+//        order -> member 지연 로딩 조회 N 번 -> 영속성 컨텍스트에 있나 확인함, 없는 경에만 DB를 찌르므로 최악의 경우 N번
+//        order -> delivery 지연 로딩 조회 N 번
+//        예) order의 결과가 4개면 최악의 경우 1 + 4 + 4번 실행된다.(최악의 경우)
+//        지연로딩은 영속성 컨텍스트에서 조회하므로, 이미 조회된 경우 쿼리를 생략한다.
 
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+        return result;
+
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderSatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderSatus = order.getStatus();
+            orderDate = order.getOrderDate();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 
 }
